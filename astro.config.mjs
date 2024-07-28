a362c894224e7c9cd745813433f2920c57efcfa8
Magnus Alexander Strømseng <magnus.stromseng@gmail.com>
2024-07-28 18:30:17 +0200

    configure sanity for astro static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
+
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel()
+    output: "server",
+    adapter: vercel(),
+    integrations: [
+        sanity({
+            projectId: "xxc5ksdw",
+            dataset: "production",
+            // Set useCdn to false if you're building statically.
+            useCdn: false,
+        }),
+        react(),
+    ],
 });
