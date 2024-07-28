7cccfbb6470d595f41c8398f1dd62eda7050edae
Magnus Alexander Strømseng <magnus.stromseng@gmail.com>
2024-07-28 20:32:25 +0200

    astro i tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,19 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
-
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    output: "server",
-    adapter: vercel(),
-    integrations: [
-        sanity({
-            projectId: "xxc5ksdw",
-            dataset: "production",
-            // Set useCdn to false if you're building statically.
-            useCdn: false,
-            studioBasePath: "/admin",
-        }),
-        react(),
-    ],
+  output: "server",
+  adapter: vercel(),
+  integrations: [sanity({
+    projectId: "xxc5ksdw",
+    dataset: "production",
+    // Set useCdn to false if you're building statically.
+    useCdn: false,
+    studioBasePath: "/admin"
+  }), react(), tailwind()]
 });
