a369fca1f25aa6bfd422fb2f69b6e0fbf3036ee4
Magnus Strømseng <magnus.stromseng@gmail.com>
2024-08-12 00:12:49 +0200

    wip: prettier fix, background images

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -2,12 +2,4 @@
 /** @type {import("prettier").Config} */
 export default {
   plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
-  overrides: [
-    {
-      files: "*.astro",
-      options: {
-        parser: "astro",
-      },
-    },
-  ],
 };
