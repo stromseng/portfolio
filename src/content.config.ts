72696d881efb7215bd44f933d9a5ae59c3f88f4d
Magnus Strømseng <magnus.stromseng@gmail.com>
2025-11-25 23:06:32 +0900

    remove updatedDate from blog posts as we now use lastmod

--- src/content.config.ts
+++ src/content.config.ts
@@ -11,7 +11,6 @@ const blog = defineCollection({
     title: z.string(),
     description: z.string(),
     pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
     tags: z.array(z.string()).optional(),
   }),
 });
